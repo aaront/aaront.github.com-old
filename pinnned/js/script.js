24d313d2165b397199492d0901ff541a2d6049b6
Aaron Toth <atoth89@gmail.com>
2011-06-30 23:30:11 -0400

    Added pinnned page

---
+++ pinnned/js/script.js
@@ -0,0 +1,12 @@
+/* Author: Aaron Toth
+
+*/
+$(function() {
+	$('#screenshots-slider').bxSlider({
+		auto: true,
+		pager: true,
+		autoHover: true,
+		pagerLocation: 'bottom',
+		controls: false
+	});
+});
